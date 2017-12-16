dd39814fdda2883711e7492284bbf3924b56b0b9
Liam Dunbar <33041060+LiamDunbar@users.noreply.github.com>
2017-12-15 19:57:17 -0800

    Helpful links to get you started!

--- webscrapper.py
+++ webscrapper.py
@@ -1,1 +1,3 @@
-
+# https://www.dataquest.io/blog/web-scraping-tutorial-python/ <------ Tutorial on Web Scraping
+# https://jamesbvaughan.com/python-twilio-scraping/ <------ Tutorial for Twilio
+# https://github.com/clevelandhighschoolcs/p4mawpup-hunterjkey/blob/master/Version%203.py <----- Good Example to go off of
